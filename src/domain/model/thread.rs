507e21c19f82a619af5af511188d26085a0c9caa
Ted Song <kitae040522@gmail.com>
2025-02-01 14:58:46 +0000

    FIX: Thread Model Field 수정

--- src/domain/model/thread.rs
+++ src/domain/model/thread.rs
@@ -5,6 +5,8 @@ use sqlx::FromRow;
 #[derive(Debug, Clone, FromRow, Deserialize, Serialize)]
 pub struct Thread {
     pub id: i64,
+    pub user_id: i64,
+
     pub title: Option<String>,
     pub content: String,
     pub parent_thread: i64,
